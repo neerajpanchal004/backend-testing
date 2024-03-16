58bfd144b4a49a4e39d7f8a362f3731bec6157da
ankitpanchal534 <ankitpanchal534@gmail.com>
2024-03-16 21:09:56 +0530

    fix

--- src/pages/api/getUser.js
+++ src/pages/api/getUser.js
@@ -1,10 +1,13 @@
 export default function handler(req, res) {
   const { category } = req.query;
-  if (category == "user") {
-    res.status(200).json({ text: "user selected" });
+  const r_obj = {
+    user: "Hello User",
+    dashbord: "Hello Dashboard",
+  };
+  if (!r_obj[category]) {
+    res.status(200).json({ text: r_obj[category] });
+  } else {
+    res.status(400).json({ text: "Invalid Category id" });
   }
-  if (category == "dashboard") {
-    res.status(200).json({ text: "dashboard selected" });
-  }
-  res.status(200).json({ text: category });
+  res.status(500).json({ text: "Something went wrong" });
 }
