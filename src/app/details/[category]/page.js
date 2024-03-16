4e50aabc84fb575f6b8365ed880a8c106734c906
ankitpanchal534 <ankitpanchal534@gmail.com>
2024-03-16 20:59:15 +0530

    fix

--- src/app/details/[category]/page.js
+++ src/app/details/[category]/page.js
@@ -1,23 +1,21 @@
-"use client"
-import Dashboard from '@/app/Dashboard';
-import User from '@/app/User';
-import { useParams } from 'next/navigation'
-import React from 'react'
+"use client";
+import Dashboard from "@/app/Dashboard";
+import User from "@/app/User";
+import { useParams } from "next/navigation";
+import React from "react";
 
 const Page = () => {
-    const router =useParams();
-    const {category}=router;
-    console.log(category)
-    if(category =="dashbord"){
-      return <Dashboard/>
-    }
-    if(category =="user"){
-      return <User/>
-    }
-    
-  return (
-    <div>Invalid</div>
-  )
-}
+  const router = useParams();
+  const { category } = router;
+  console.log(category);
+  if (category == "dashbord") {
+    return <Dashboard />;
+  }
+  if (category == "user") {
+    return <User />;
+  }
 
-export default Page
+  return <div>Invalid</div>;
+};
+
+export default Page;
--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,10 +1,10 @@
 export default function handler(req, res) {
   const { category } = req.query;
   if (category == "dashbord") {
-    res.status(200).json({ success_message: "Dashboard Category Selected!" });
+    res.status(200).json({ message: "Dashboard Category Selected!" });
   }
   if (category == "user") {
-    res.status(200).json({ success_message: "User Category Selected!" });
+    res.status(200).json({ message: "User Category Selected!" });
   }
   res.status(400).json("Invalid Category! Please Select 'dashbord' or 'user'.");
 }
