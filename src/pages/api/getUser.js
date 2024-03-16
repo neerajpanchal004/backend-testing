1daf25cae189bee14c32b57b9145fac0ec06d34c
ankitpanchal534 <ankitpanchal534@gmail.com>
2024-03-16 20:50:26 +0530

    fix

--- src/pages/api/getUser.js
+++ src/pages/api/getUser.js
@@ -1,4 +1,10 @@
 export default function handler(req, res) {
   const { category } = req.query;
+  if (category == "user") {
+    res.status(200).json({ text: "user selected" });
+  }
+  if (category == "dashboard") {
+    res.status(200).json({ text: "dashboard selected" });
+  }
   res.status(200).json({ text: category });
 }
